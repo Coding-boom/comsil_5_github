e80b10fd0ae2c9c8c1c92951d2e4cef3e6bfdc19
Jaebeom <thswoqja98@naver.com>
2023-04-11 23:34:56 +0900

    assignment

--- print.c
+++ print.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 
 int main(){
-<<<<<<< HEAD
     printf("name : Kate\n");
-    printf("gender : male\n");
-=======
-    printf("name : James\n");
     printf("gender : female\n");
->>>>>>> branch2
     printf("major : math");
 }
